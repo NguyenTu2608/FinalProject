843fa43f3801f0266dfd91937a24cdd8412838a6
NguyenTu2608 <atuisme2608@gmail.com>
2025-02-15 20:23:32 +0700

    1
    1

--- api/demo/src/main/java/com/example/demo/repositories/ChessGameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.ChessGame;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChessGameRepository extends MongoRepository<ChessGame, String> {
-}
--- chinesechess/chinesechess/src/test/java/com/example/chinesechess/ChinesechessApplicationTests.java
+++ chinesechess/chinesechess/src/test/java/com/example/chinesechess/ChinesechessApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.chinesechess;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ChinesechessApplicationTests {
 
 	@Test
 	void contextLoads() {

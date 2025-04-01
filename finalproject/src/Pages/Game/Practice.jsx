e41b5afd3ac8b71b53cf7e91d5a856dedc1a374a
26thang8 <atuisme2608@gmail.com>
2025-04-01 15:58:51 +0700

    Revert "1"

--- finalproject/src/Pages/Game/Practice.jsx
+++ finalproject/src/Pages/Game/Practice.jsx
@@ -25,7 +25,7 @@ const Pratice = () => {
         </button>
 
         <button
-          onClick={() => navigate("/Practice/AIRoom")}
+          onClick={() => navigate("/Practice/tim-phong")}
           className="w-full py-4 bg-green-600 rounded-lg text-xl font-semibold transition duration-300 hover:bg-green-700 shadow-md"
         >
           🔍 Chơi với AI
